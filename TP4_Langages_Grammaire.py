d0178a48047d2ea2ccfb99ef2532671311bfac08
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-12-18 11:29:57 +0100

    Update TP4_Langages_Grammaire.py

--- TP4_Langages_Grammaire.py
+++ TP4_Langages_Grammaire.py
@@ -50,19 +50,53 @@ Manipulation de données
 import ply.yacc as yacc
 import TP4_Langages_Lexique
 
-
+tokens = TP4_Langages_Lexique.tokens
 
 # =============================================================================
 # Parser
 # =============================================================================
 
-def p_assign(p):
-    '''assign : NAME EQUALS expr'''
+#for element in [a + b + c for a in ['contrainte', 'contrainte_liee'] for b in [' AND ', ' OR '] for c in ['contrainte', 'contrainte_liee']]: print(element)
+
+#def p_assign(p):
+#    '''assign : NAME EQUALS'''
 
 def p_get(p):
-    '''get : GET URL'''
+    '''get : GET URL 
+    | GET URL contrainte
+    | GET URL contrainte_liee
+    '''
+    print('get')
+    
+def p_contrainte(p):
+    '''contrainte : CONTAINS NAME
+    | EXCLUDES NAME
+    '''
+    print('contrainte')
 
-   
+def p_contrainte_liee(p):
+    '''contrainte_liee : contrainte AND contrainte 
+    | contrainte AND contrainte_liee 
+    | contrainte OR contrainte 
+    | contrainte OR contrainte_liee 
+    | contrainte_liee AND contrainte 
+    | contrainte_liee AND contrainte_liee 
+    | contrainte_liee OR contrainte 
+    | contrainte_liee OR contrainte_liee'''
+    print('contrainte_liee')
+
+#def p_error(p):
+#    pass
+
+yacc.yacc() # build the parser
+
+yacc.parse(TP4_Langages_Lexique.data) 
+
+
+
+
+
+
 #
 #
 #def p_assign(p):
@@ -87,6 +121,36 @@ def p_get(p):
 #
 #data = "x = 3 * 4 + 5 * 6"
 #yacc.parse(data)
+
+
+
+# =============================================================================
+# Variables de test
+# =============================================================================
+
+#Programme Idule initial :
+test0 = 'get http://www.machin-truc.org/page.html'
+ 
+#Programme Idule initial avec contrainte :
+test1 = 'get http://www.machin-truc.org/page.html contains toto'
+
+#Programme Idule initial avec groupe de contraintes :
+test2 = 'get http://www.machin-truc.org/page.html contains toto and exclude titi or contains blabla'
+test3 = 'get http://www.bidule.org/page.html contains bli or contains blu and exclude blo'
+
+#Programme Idule initial avec stats :
+test4 = 'r1 = get http://www.machin-truc.org/page.html contains toto and exclude titi or contains blabla'
+test5 = 'r2 = get http://www.bidule.org/page.html contains bli or contains blu and exclude blo'
+test6 = 'stat r1 union r2 contains Trump and exclude Clinton contains Clinton and exclude Trump contains Trump and contains Clinton'
+test7 = 'stat r1 intersect r2 contains Trump and contains Clinton'
+
+
+# =============================================================================
+# Tests
+# =============================================================================
+
+#Tests du lexer
+
 
 
 
